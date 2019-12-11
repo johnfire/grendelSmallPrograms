b83562251dec0150050a379ef3f5fbd1e7c8f995
Christopher Rehm <christopherrehm@web.de>
2019-12-11 21:34:42 +0100

    updates for msg passing

--- zerocameraprogram.py
+++ zerocameraprogram.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import json
+
 from picamera import PiCamera
 from time import sleep
 from datetime import datetime
@@ -9,20 +11,24 @@ def cameraZeroTakePhotos(cameraRunTime = 60, pauseTime =10):
 
     camera1Run=True
     camera = PiCamera()
-
+    
     while(camera1Run==True):
-        print("taking pic cam2 now")
+        #print("taking pic cam2 now")
+        foto = 'zero' + str(datetime.now()) +'.jpg'
         #camera.capture('/media/grendelData102/testworkspace/imagesPiCam2/'+str(datetime.now())+'.jpg')
-        camera.capture('/home/pi/Desktop/ZeroImages/'+str(datetime.now())+'.jpg')
-        sleep(pauseTime)
-        cameraRunTime-=pauseTime
-        if (cameraRunTime <= 0):
+        camera.capture('/home/pi/Desktop/ZeroImages/' + foto)
+        message = [ 'newfoto', foto , str(datetime.now()) , 'zero' ] 
+        with open('msg' + str(datetime.now()) + '.msg', 'w') as outfile:
+            json.dump(message, outfile)
+        #sleep(pauseTime)
+        #cameraRunTime-=pauseTime
+        #if (cameraRunTime <= 0):
             camera1Run = False
 
 
 if __name__ == '__main__':
 
-    cameraZeroTakePhotos(30,5)
+    cameraZeroTakePhotos(2,.2)
 
     
 
