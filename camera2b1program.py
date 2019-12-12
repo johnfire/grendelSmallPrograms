ccdad2af04866d77502a341b666dac7afb80c7c0
Christopher Rehm <christopherrehm@web.de>
2019-12-12 20:40:29 +0100

    minor changes for speed testing

--- camera2b1program.py
+++ camera2b1program.py
@@ -12,12 +12,12 @@ def camera2b1TakePhotos(cameraRunTime =60, pauseTime=10):
 
     while(camera1Run==True):
         print("taking pic cam1 now")
-        #camera.capture('/media/grendelData102/GrendelData/grendelFotos/'+str(datetime.now())+'.jpg')
-        camera.capture('/home/pi/Desktop/2b1images/'+str(datetime.now())+'.jpg')
-        sleep(pauseTime)
-        cameraRunTime-=pauseTime
-        if (cameraRunTime < 0):
-            camera1Run = False
+        camera.capture('/media/grendelData102/GrendelData/grendelFotos/'+str(datetime.now())+'.jpg')
+        #camera.capture('/home/pi/Desktop/2b1images/'+str(datetime.now())+'.jpg')
+        #sleep(pauseTime)
+        #cameraRunTime-=pauseTime
+        #if (cameraRunTime < 0):
+            #camera1Run = False
 
 if __name__ == "__main__":
     camera2b1TakePhotos(15,3)
