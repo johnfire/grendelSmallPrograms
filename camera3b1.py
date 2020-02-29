562c85ecbe38bf6f4c503cb25a86376e0ee355fd
Christopher Rehm <christopherrehm@web.de>
2020-02-29 14:23:30 +0100

    new camera programs

--- camera3b1.py
+++ camera3b1.py
@@ -3,7 +3,7 @@ from picamera import PiCamera
 from time import sleep
 from datetime import datetime
 
-def cameraTakePhotos(cameraRunTime =60, pauseTime=10):
+def camera2b1TakePhotos(cameraRunTime =60, pauseTime=10):
     print("hello world from the 2b1 cam function")
 
     camera1Run=True
@@ -13,14 +13,14 @@ def cameraTakePhotos(cameraRunTime =60, pauseTime=10):
     while(camera1Run==True):
         print("taking pic cam1 now")
         #camera.capture('/media/grendelData102/GrendelData/grendelFotos/'+str(datetime.now())+'.jpg')
-        camera.capture('/media/grendelData102/GrendelData/grendelFotos/'+"N"+str(datetime.now())+'.jpg')
+        camera.capture('/media/grendelData102/GrendelData/grendelFotos/'+str(datetime.now())+'.jpg')
         sleep(pauseTime)
         cameraRunTime-=pauseTime
         if (cameraRunTime < 0):
             camera1Run = False
 
 if __name__ == "__main__":
-    cameraTakePhotos(15,3)
+    camera2b1TakePhotos(15,3)
     
 
 
